c4b301efe1f3b85c71b942226292dc7f430d0c1b
Odunosh Moses <Odunoshomoses@gmail.com>
2022-08-30 16:07:21 +0000

    setup and structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Landing from './pages/Landing';
+
+function App() {
+  return (
+    <>
+      <Landing />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./header.css"
+
+function Header() {
+  return (
+    <div>
+        <h1>Pair</h1>
+    </div>
+  )
+}
+
+export default Header;
