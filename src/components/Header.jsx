40ac4ca61951e9271663f15c8d7abb4e66101fca
Odunosh Moses <Odunoshomoses@gmail.com>
2022-09-03 23:04:24 +0000

    [feat]:mobile responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./header.css"
+import {GiHamburgerMenu} from 'react-icons/gi'
 
 function Header() {
+  const [show, setShow] = useState(true)
+
+  const showMenu = () => {
+    setShow(!show)
+  }
   return (
     <div className='header'>
       <div className='header__logo'>
         <h1>Pair</h1>
       </div>
       <div className='header__links'>
-          <h4 className='header__text'><a href='../pages/About'>About</a></h4>
-          <h4 className='header__text'><a href='../pages/Mission'>Mission</a></h4>
-          <h4 className='header__text'><a href='../pages/Products'>Products</a></h4>
+        <button onClick={showMenu}>
+          <GiHamburgerMenu id='hamburger'/>
+        </button>
+          <h4 className={show ? 'header__text' : null}><a href='../pages/About'>About</a></h4>
+          <h4 className={show ? 'header__text' : null}><a href='../pages/Mission'>Mission</a></h4>
+          <h4 className={show ? 'header__text' : null}><a href='../pages/Products'>Products</a></h4>
       </div>
-      <div className='header__buttons'>
+      <div className={show ? 'header__buttons' : null}>
           <button className='login'>Login</button>
           <button className='signup'>Sign Up</button>
       </div>
-    </div>
+      </div>
   )
 }
 
